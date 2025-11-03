ac04ac926ba6ef34f931353df67daf4142fb7865
Dulakshi <dulakshi.thariduka@innerspace.eu>
2025-11-03 16:15:42 +0100

    use customized locator

--- pages/projmanagement/project.page.ts
+++ pages/projmanagement/project.page.ts
@@ -10,8 +10,8 @@ export class ProjectPage {
   async createProject(projectName: string, customerName: string) {
     await this.page.getByRole('button', { name: 'add_circle_outline Create' }).click();
     await this.page.getByRole('textbox', { name: 'Project Name' }).fill(projectName);
-    await this.page.locator('div').filter({ hasText: /^Groninger$/ }).first().click();
-    await this.page.getByRole('combobox', { name: 'Selected Groninger. Customer' }).fill(customerName);
+    await this.page.getByRole('textbox', { name: 'Project Name' }).press('Tab');
+    await this.page.getByRole('combobox', { name: /Selected .* Customer/ }).fill(customerName);
     await this.page.getByTestId('stSelectboxVirtualDropdown').getByText(customerName).click();
     await this.page.getByTestId('stBaseButton-primary').click();
   }
@@ -21,5 +21,10 @@ export class ProjectPage {
     await this.page.getByRole('combobox').fill(projectName);
     await this.page.getByTestId('stSelectboxVirtualDropdown').getByText(projectName).click();
   }
+  
+  
+  
+  
+  
 
 }
